78f85cbde13cfdc9d7b5f843cbc87ffb6b0ba0fb
berlinway <45029945+berlinway@users.noreply.github.com>
2021-05-06 22:20:01 +0800

    JUST TEST

--- test.sh
+++ test.sh
@@ -26,15 +26,20 @@ cat > ~/.config/rclone/rclone.conf <<EOF
 type = drive
 scope = drive
 token = {"access_token":"ya29.a0AfH6SMAoZ-vLliokqCsq9mZViiI-ZtRlCk6MgSPG5XzhRQgQNESWsHI08wLpRdDz0r-j5ENZ6OzfPLnz6LpmMjGHiAIvmrnzzV8L0jmyGYLizaJBhdCv7DpnrdTkksXnVj9ZHVazMSMW-i897jB4saCuYU5E","token_type":"Bearer","refresh_token":"1//0ddhud8rBEtfGCgYIARAAGA0SNwF-L9IrS47c9ipr-FoKYLAMrx_sFa8R5vsxuRJzlperrmsu5Gc_SFJP0xuzG72bX-xLsPvJbwU","expiry":"2021-05-06T10:45:02.018913698Z"}
+
+[gd-iokle]
+type = drive
+scope = drive
+token = {"access_token":"ya29.a0AfH6SMCxXNCL1eV0CDOH0xaEiq5taen2ZNIO99y8n-jxd6fNe8G-knac0f-JF1l9WGeYOo9rc9nEF9WqQY-pmS6J08_g4buOZptE4pgBf1UWS5YFPFfZuVV1KiHL_x-UVuyiPEJ_zYMMeYCGfGWIbPvGYwW2","token_type":"Bearer","refresh_token":"1//0dtgwgv63wbY8CgYIARAAGA0SNwF-L9IrYLOnoCFmUEddMQkyh5_H6iifoWHFair4m26_VSuSyZ3LeAjSw0oRyNG8PJfwS_ysVUk","expiry":"2021-05-06T15:06:14.400217312Z"}
 EOF
 
-cat >/etc/systemd/system/rclone-gd.service<<EOF
+cat >/etc/systemd/system/rclone.service<<EOF
 [Unit]
-Description = rclone-gd
+Description = rclone-iokle
      
 [Service]
 User = root
-ExecStart = /usr/bin/rclone mount gd-yun: /app/gd  --vfs-cache-max-age 2h  --file-perms 0777 --copy-links --no-gzip-encoding --no-check-certificate --allow-other --allow-non-empty --umask 000 
+ExecStart = /usr/bin/rclone mount gd-iokle: /app/gd  --vfs-cache-max-age 2h  --file-perms 0777 --copy-links --no-gzip-encoding --no-check-certificate --allow-other --allow-non-empty --umask 000 
 Restart = on-abort
      
 [Install]
@@ -43,8 +48,8 @@ WantedBy = multi-user.target
 EOF
 
 
-systemctl enable rclone-gd
-systemctl start rclone-gd
+systemctl enable rclone
+systemctl start rclone
 
 ##########################################
 ###########     2、附加500ssd      #######
@@ -52,12 +57,12 @@ systemctl start rclone-gd
 
 #附加去后台附加，默认是开机器的时候就加上去了
 #Create new empty partitions:
-parted -s /dev/vdb mklabel gpt
-parted -s /dev/vdb unit mib mkpart primary 0% 100%
+#parted -s /dev/vdb mklabel gpt
+#parted -s /dev/vdb unit mib mkpart primary 0% 100%
 #Create new empty filesystem
-mkfs.ext4 /dev/vdb1
+#mkfs.ext4 /dev/vdb1
 #默认不关机，随意挂载
-mount /dev/vdb1   /app/storage
+#mount /dev/vdb1   /app/storage
 
 
 
@@ -74,34 +79,11 @@ rm  -rf  __MACOSX
 
 
 
-cat > /app/hpool/chia-plotter/p1.sh<<EOF
-#!/bin/bash
 
-for((i=0;i<1000;i++))
-do
-	echo $i  "start" `date` >> /app/hpool/chia-plotter/p1.log
-	/app/hpool/chia-plotter/chia-plotter-linux-amd64 -action plotting -plotting-fpk 0x8bdadf4c2052ca2e1c3113d9c2752e835ab0f669113c961f85bd945ea3d1184225b49cb3595f5a59918d94bfb7c4809d -plotting-ppk 0x86a0265e3c0de8d5db9a351afe5d77ed268cfbeca0a6f89a1a097d8599aaf296ee40fb264cbe5bec2e520a84865c2ded -plotting-n 2 -b 3390 -d   /app/gd -t /app/tmp
-	echo $i  "start" `date` >> /app/hpool/chia-plotter/p1.log
-done
-
-EOF
-
-cat > /app/hpool/chia-plotter/p2.sh<<EOF
-#!/bin/bash
-
-sleep 3h
-for((i=0;i<1000;i++))
-do
-	echo $i  "start" `date` >> /app/hpool/chia-plotter/p2.log
-	/app/hpool/chia-plotter/chia-plotter-linux-amd64 -action plotting -plotting-fpk 0x8bdadf4c2052ca2e1c3113d9c2752e835ab0f669113c961f85bd945ea3d1184225b49cb3595f5a59918d94bfb7c4809d -plotting-ppk 0x86a0265e3c0de8d5db9a351afe5d77ed268cfbeca0a6f89a1a097d8599aaf296ee40fb264cbe5bec2e520a84865c2ded -plotting-n 2 -b 3390 -d   /app/gd -t /app/storage
-	echo $i  "start" `date` >> /app/hpool/chia-plotter/p2.log
-done
-
-EOF
 touch /app/hpool/chia-plotter/p1.log
 touch  /app/hpool/chia-plotter/p2.log
-chmod +x /app/hpool/chia-plotter/p1.sh
-chmod +x /app/hpool/chia-plotter/p2.sh
+#chmod +x /app/hpool/chia-plotter/p1.sh
+#chmod +x /app/hpool/chia-plotter/p2.sh
 
 
 
